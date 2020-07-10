e85dafcefc9cf832c0cb240766518de6b4c422b6
harunsasmaz <hsasmaz16@ku.edu.tr>
2020-07-10 21:11:39 +0300

    elementary pointer functions added

---
+++ collector.c
@@ -0,0 +1,67 @@
+#include "collector.h"
+
+static size_t gc_hash(void *ptr) 
+{
+    return ((uintptr_t)ptr) >> 3;
+}
+
+static size_t gc_probe(gc_t* gc, size_t i, size_t h)
+{
+    long v = i - (h-1);
+    if (v < 0) { v = gc->nslots + v; }
+    return v;
+}
+
+static gc_ptr_t *gc_get_ptr(gc_t *gc, void *ptr) 
+{
+    size_t i, j, h;
+    i = gc_hash(ptr) % gc->nslots; j = 0;
+    while (1) 
+    {
+        h = gc->items[i].hash;
+        if (h == 0 || j > gc_probe(gc, i, h)) return NULL;
+        if (gc->items[i].ptr == ptr) return &gc->items[i];
+        i = (i+1) % gc->nslots; 
+        j++;
+    }
+    return NULL;
+}
+
+static void gc_add_ptr(gc_t *gc, void *ptr, size_t size, 
+    int flags, void(*dtor)(void*)) 
+{
+    gc_ptr_t item, tmp;
+    size_t h, p, i, j;
+
+    i = gc_hash(ptr) % gc->nslots; j = 0;
+
+    item.ptr = ptr;
+    item.flags = flags;
+    item.size = size;
+    item.hash = i+1;
+    item.dtor = dtor;
+
+    while (1) 
+    {
+        h = gc->items[i].hash;
+        if (h == 0) 
+        { 
+            gc->items[i] = item; 
+            return; 
+        }
+
+        if (gc->items[i].ptr == item.ptr) return; 
+        p = gc_probe(gc, i, h);
+
+        if (j >= p) 
+        {
+            tmp = gc->items[i];
+            gc->items[i] = item;
+            item = tmp;
+            j = p;
+        }
+
+        i = (i+1) % gc->nslots; 
+        j++;
+    }
+}
